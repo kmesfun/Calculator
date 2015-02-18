3696a56ee8ba982101b799bd8a6de906fbf8c8e6
kmesfun <kmess024@gmail.com>
2015-02-17 21:51:47 -0800

    calculator
    first swift app (iOS)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    var isTypingNumber = false
+    var firstNumber = 0
+    var secondNumber = 0
+    var operation = ""
+
+
+    
+    @IBAction func numberTapped(sender: AnyObject) { //number
+        var number = sender.currentTitle
+        
+        if isTypingNumber {
+            calculatorDisplay.text = calculatorDisplay.text! + number!!
+        }else{
+            calculatorDisplay.text = number
+            isTypingNumber = true
+        }
+        
+    }
+        
+    
+    @IBAction func calculationTapped(sender: AnyObject) { //calculation
+        isTypingNumber = false
+        firstNumber = calculatorDisplay.text!.toInt()!
+        operation = sender.currentTitle!!
+    }
+    
+    @IBAction func equalsTapped(sender: AnyObject) { //equals
+        isTypingNumber = false
+        var result = 0
+        secondNumber = calculatorDisplay.text!.toInt()!
+        
+        if operation == "+" {
+            result = firstNumber + secondNumber
+        } else if operation == "-" {
+        result = self.firstNumber - self.secondNumber
+        }else if operation == "*" {
+            result = self.firstNumber * self.secondNumber
+        }else if operation == "/" {
+            result = self.firstNumber / self.secondNumber
+        }
+        
+        calculatorDisplay.text = "\(result)"
+            
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
